d1053f8a825c6e312d7a4c0836213aba4057c624
3Vivek <vivek6972gkp@gmail.com>
2024-09-19 14:33:27 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { CommonModule } from '@angular/common'; // Import CommonModule
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import {NavbarComponent} from "./navbar/navbar.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, NavbarComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'AngularApp';
+  showComponent = false;
+  ngOnInit(): void {
+    initFlowbite();
+  }
+  renderComponent() {
+    this.showComponent = !this.showComponent; // Toggle the value
+  }
 }
